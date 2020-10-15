a28fa7cb7c697307aeadb43b24e11bd8fc3b4d72
Salvador Olivar <57721519+SalvadorOlivar@users.noreply.github.com>
2020-10-15 01:46:24 -0300

    Update NumeroMagico.js

--- NumeroMagico.js
+++ NumeroMagico.js
@@ -1,15 +1,24 @@
 let numeroMagico = 20;
 let numeroUsuario = parseInt(prompt("Cual es el numero magico?"));
 
-if (numeroUsuario == numeroMagico){
+if (numeroMagico !== numeroUsuario) {
+  console.log("Intenta denuevo!")
+  while (numeroMagico !== numeroUsuario) {
+    
+    let nuevoNumeroUsuario = parseInt(prompt("Cual es el numero magico?"));
 
-  console.log("Adivinaste!!!!!!");
+    if (numeroMagico == nuevoNumeroUsuario) {
+      console.log("adivinaste")
 
-} else if (numeroUsuario < numeroMagico){
-  
-  console.log("El numero magico es Mayor");
+    } else if(numeroMagico < nuevoNumeroUsuario ) {
+      console.log("el numero magico es menor")
 
-} else if (numeroUsuario > numeroMagico){
+    } else if(numeroMagico > nuevoNumeroUsuario){
+      console.log("el numero magico es mayor")
 
-  console.log("El numero magico es Menor");
+    }
+     numeroUsuario = nuevoNumeroUsuario
+  }
+} else if (numeroMagico == numeroUsuario){
+  console.log("adivinaste");
 }
