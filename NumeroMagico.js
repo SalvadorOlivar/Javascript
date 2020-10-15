438b5d76b27fc6070a422a050ad05e1bd31aa20a
Salvador Olivar <57721519+SalvadorOlivar@users.noreply.github.com>
2020-10-15 01:13:57 -0300

    Create NumeroMagico.js

---
+++ NumeroMagico.js
@@ -0,0 +1,15 @@
+let numeroMagico = 20;
+let numeroUsuario = parseInt(prompt("Cual es el numero magico?"));
+
+if (numeroUsuario == numeroMagico){
+
+  console.log("Adivinaste!!!!!!");
+
+} else if (numeroUsuario < numeroMagico){
+  
+  console.log("El numero magico es Mayor");
+
+} else if (numeroUsuario > numeroMagico){
+
+  console.log("El numero magico es Menor");
+}
